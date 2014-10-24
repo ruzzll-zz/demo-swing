1e4876885e6a109486001badf2af2fdb8b92276c
ruzzll <ruzzll@hotmail.com>
2014-10-24 17:37:35 -0500

    send eclipse configuration

--- src/main/java/com/swing/demo/app/Main.java
+++ src/main/java/com/swing/demo/app/Main.java
@@ -4,13 +4,21 @@ import javax.swing.SwingUtilities;
 
 public class Main {
 	
-	public static void main(final String[] args){
+	public static void main(String[] args){
 		System.out.print("asdasd");
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			
 			public void run() {
 				Application app = new Application();
+				app.setTitle("-:: Demo ::-");
+				app.setSize(800, 600);
+				app.setLocationRelativeTo(null);
+				app.setDefaultCloseOperation(Application.EXIT_ON_CLOSE);
+				
+				app.setVisible(true);
 			}
+			
 		});
 	}
 
