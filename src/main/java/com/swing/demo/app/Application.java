c038fd8ee95e46f2238b278004b256fed00f460a
ruzzll <ruzzll@hotmail.com>
2014-10-28 16:20:34 -0500

    test test test

--- src/main/java/com/swing/demo/app/Application.java
+++ src/main/java/com/swing/demo/app/Application.java
@@ -1,9 +1,28 @@
 package com.swing.demo.app;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 
 public class Application extends JFrame {
+	public Application() {
+		
+		JToolBar toolBar = new JToolBar();
+		getContentPane().add(toolBar, BorderLayout.NORTH);
+		
+		JButton btnAsdasd = new JButton("asdasd");
+		btnAsdasd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		toolBar.add(btnAsdasd);
+	}
 
 	/**
 	 * 
--- src/main/java/com/swing/demo/app/Main.java
+++ src/main/java/com/swing/demo/app/Main.java
@@ -13,9 +13,14 @@ public class Main {
 				Application app = new Application();
 				app.setTitle("-:: Demo ::-");
 				app.setSize(800, 600);
-				app.setExtendedState(app.MAXIMIZED_BOTH);  
+				//app.setExtendedState(app.MAXIMIZED_BOTH);  
 				app.setLocationRelativeTo(null);
 				app.setDefaultCloseOperation(Application.EXIT_ON_CLOSE);
+				
+				/*URL url = ClassLoader.getSystemResource("src/main/resources/Camera.jpg");
+				Toolkit kit = Toolkit.getDefaultToolkit();
+				Image img = kit.createImage(url);
+				app.setIconImage(img);*/
 				
 				app.setVisible(true);
 			}
